46e85ed3c92686edf92908a34b63d474e2c58b6d
Jakub Kicinski <kubakici@wp.pl>
2013-01-19 20:42:13 +0100

    Dodanie pozostałych plików projektu

---
+++ garden_pl.ts
@@ -0,0 +1,405 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl_PL">
+<context>
+    <name>AddNew</name>
+    <message>
+        <location filename="addnew.ui" line="14"/>
+        <source>Form</source>
+        <translation>Formularz</translation>
+    </message>
+    <message>
+        <location filename="addnew.ui" line="32"/>
+        <source>Add new</source>
+        <translation>Dodaj nowy</translation>
+    </message>
+    <message>
+        <location filename="addnew.ui" line="49"/>
+        <source>Pictures:</source>
+        <translation>Obrazy:</translation>
+    </message>
+    <message>
+        <location filename="addnew.ui" line="88"/>
+        <source>Click on image to set as main. Double click to enlarge.</source>
+        <translation>Kliknij na obrazku i zapisz zmiany, aby uczynić go domyślnym. Kliknij dwukrotnie, by powiększyć obrazek.</translation>
+    </message>
+    <message>
+        <location filename="addnew.ui" line="121"/>
+        <source>Add</source>
+        <translation>Dodaj</translation>
+    </message>
+    <message>
+        <location filename="addnew.ui" line="132"/>
+        <source>Remove</source>
+        <translation>Usuń</translation>
+    </message>
+    <message>
+        <location filename="addnew.ui" line="152"/>
+        <source>Data:</source>
+        <translation>Dane:</translation>
+    </message>
+    <message>
+        <location filename="addnew.ui" line="170"/>
+        <source>Name:</source>
+        <translation>Nazwa:</translation>
+    </message>
+    <message>
+        <location filename="addnew.ui" line="180"/>
+        <source>Type:</source>
+        <translation>Typ:</translation>
+    </message>
+    <message>
+        <location filename="addnew.ui" line="200"/>
+        <source>Flavour:</source>
+        <translation>Zapach:</translation>
+    </message>
+    <message>
+        <location filename="addnew.ui" line="210"/>
+        <source>Flowering time:</source>
+        <translation>Pora kwitnienia:</translation>
+    </message>
+    <message>
+        <location filename="addnew.ui" line="220"/>
+        <source>Bush size:</source>
+        <translation>Wielkość krzewów:</translation>
+    </message>
+    <message>
+        <location filename="addnew.ui" line="230"/>
+        <source>Frost resistance:</source>
+        <translation>Mrozoodporność:</translation>
+    </message>
+    <message>
+        <location filename="addnew.ui" line="240"/>
+        <source>Description:</source>
+        <translation>Opis:</translation>
+    </message>
+    <message>
+        <source>New attribute:</source>
+        <translation type="obsolete">Nowy atrybut:</translation>
+    </message>
+    <message>
+        <location filename="addnew.ui" line="190"/>
+        <source>Flowers:</source>
+        <translation>Kwiaty:</translation>
+    </message>
+    <message>
+        <source>Special attributes:</source>
+        <translation type="obsolete">Atrybuty specjalne:</translation>
+    </message>
+    <message>
+        <location filename="addnew.cpp" line="73"/>
+        <source>Edit </source>
+        <translation>Edycja </translation>
+    </message>
+    <message>
+        <location filename="addnew.cpp" line="131"/>
+        <source>Removing failed</source>
+        <translation>Usuwanie nie powiodło się</translation>
+    </message>
+    <message>
+        <source>Unspecified error occured during attempt to remove picture.
+Pleasecancel edtition, reopen tab and tryagain.</source>
+        <translation type="obsolete">Podczas usuwania zdjęcia wystąpił nierozpoznany błąd.
+Proszę zapisać anulować edycję i spróbować ponownie.</translation>
+    </message>
+    <message>
+        <location filename="addnew.cpp" line="132"/>
+        <source>Unspecified error occured during attempt to remove picture.
+Please cancel edtition, reopen tab and try again.</source>
+        <translation>Wystąpił nieznany błąd podczas usuwania obrazka.
+Proszę anulować edycję i spróbować ponownie.</translation>
+    </message>
+    <message>
+        <location filename="addnew.cpp" line="145"/>
+        <source>Select garden file</source>
+        <translation>Wybierz plik z ogrodem</translation>
+    </message>
+    <message>
+        <location filename="addnew.cpp" line="147"/>
+        <source>Images (*.png *.jpg *.bmp)</source>
+        <translation>Obrazy (*.png *.jpg *.bmp)</translation>
+    </message>
+    <message>
+        <source>Add attribute</source>
+        <translation type="obsolete">Dodaj atrybut</translation>
+    </message>
+    <message>
+        <source>Please provide attribute name in
+field on the left of the button.</source>
+        <translation type="obsolete">Proszę podać nazwę atrybuty
+w polu z prawdej strony przyckisku.</translation>
+    </message>
+    <message>
+        <source>Unspecified error occured during attempt to remove attribute.
+Please cancel edtition, reopen tab and try again.</source>
+        <translation type="obsolete">Podczas usuwania atrybutu wystąpił nierozpoznany błąd.
+Proszę zapisać anulować edycję i spróbować ponownie.</translation>
+    </message>
+</context>
+<context>
+    <name>MainTable</name>
+    <message>
+        <location filename="maintable.ui" line="14"/>
+        <source>Form</source>
+        <translation>Formularz</translation>
+    </message>
+    <message>
+        <location filename="maintable.ui" line="48"/>
+        <source>Add</source>
+        <translation>Dodaj</translation>
+    </message>
+    <message>
+        <location filename="maintable.ui" line="55"/>
+        <source>Ctrl+A</source>
+        <translation>Ctrl+A</translation>
+    </message>
+    <message>
+        <location filename="maintable.ui" line="62"/>
+        <source>Remove</source>
+        <translation>Usuń</translation>
+    </message>
+    <message>
+        <location filename="maintable.cpp" line="44"/>
+        <source>Name</source>
+        <translation>Nazwa</translation>
+    </message>
+    <message>
+        <location filename="maintable.cpp" line="45"/>
+        <source>Flowers</source>
+        <translation>Kwiaty</translation>
+    </message>
+    <message>
+        <location filename="maintable.cpp" line="46"/>
+        <source>Bush size</source>
+        <translation>Wielkość krzewów</translation>
+    </message>
+    <message>
+        <location filename="maintable.cpp" line="47"/>
+        <source>Description</source>
+        <translation>Opis</translation>
+    </message>
+    <message>
+        <location filename="maintable.cpp" line="48"/>
+        <source>Photo</source>
+        <translation>Zjęcie</translation>
+    </message>
+    <message>
+        <location filename="maintable.cpp" line="49"/>
+        <source>Flavour</source>
+        <translation>Zapach</translation>
+    </message>
+    <message>
+        <location filename="maintable.cpp" line="51"/>
+        <source>Flowering</source>
+        <translation>Pora kwitnienia</translation>
+    </message>
+    <message>
+        <location filename="maintable.cpp" line="53"/>
+        <source>Frost resistance</source>
+        <translation>Mrozoodporność</translation>
+    </message>
+    <message>
+        <location filename="maintable.cpp" line="55"/>
+        <source>Type</source>
+        <translation>Typ</translation>
+    </message>
+    <message>
+        <location filename="maintable.cpp" line="66"/>
+        <source>Remove row</source>
+        <translation>Usuń wiersz</translation>
+    </message>
+    <message>
+        <location filename="maintable.cpp" line="67"/>
+        <source>Are you sure you want to remove
+this row?
+This change cannot be undone.</source>
+        <translation>Czy na pewno chcesz usunąć ten wiersz?
+Usunięcie zostanie wykonane natychmiast
+i nie będzie możliwości cofnięcia zmian.</translation>
+    </message>
+</context>
+<context>
+    <name>OBuiltInsEditor</name>
+    <message>
+        <location filename="obuiltinseditor.ui" line="14"/>
+        <source>Edit built-ins</source>
+        <translation>Edytuj wartości wbudowane</translation>
+    </message>
+    <message>
+        <location filename="obuiltinseditor.ui" line="38"/>
+        <source>Add</source>
+        <translation>Dodaj</translation>
+    </message>
+    <message>
+        <location filename="obuiltinseditor.ui" line="58"/>
+        <source>Remove</source>
+        <translation>Usuń</translation>
+    </message>
+    <message>
+        <location filename="obuiltinseditor.ui" line="69"/>
+        <location filename="obuiltinseditor.cpp" line="27"/>
+        <source>Types</source>
+        <translation>Typy</translation>
+    </message>
+    <message>
+        <location filename="obuiltinseditor.ui" line="74"/>
+        <source>Flavours</source>
+        <translation>Zapachy</translation>
+    </message>
+    <message>
+        <location filename="obuiltinseditor.ui" line="79"/>
+        <source>Flowering times</source>
+        <translation>Pory kwitnienia</translation>
+    </message>
+    <message>
+        <location filename="obuiltinseditor.ui" line="84"/>
+        <source>Frost resistance</source>
+        <translation>Mrozoodporność</translation>
+    </message>
+    <message>
+        <location filename="obuiltinseditor.ui" line="108"/>
+        <source>Close</source>
+        <translation>Zamknij</translation>
+    </message>
+    <message>
+        <location filename="obuiltinseditor.cpp" line="27"/>
+        <source>Flavour</source>
+        <translation>Zapach</translation>
+    </message>
+    <message>
+        <location filename="obuiltinseditor.cpp" line="28"/>
+        <source>Flowering</source>
+        <translation>Pora kwitnienia</translation>
+    </message>
+    <message>
+        <location filename="obuiltinseditor.cpp" line="28"/>
+        <source>Frost</source>
+        <translation>Mrozoodporność</translation>
+    </message>
+    <message>
+        <location filename="obuiltinseditor.cpp" line="36"/>
+        <source>Name</source>
+        <translation>Nazwa</translation>
+    </message>
+</context>
+<context>
+    <name>OMain</name>
+    <message>
+        <location filename="omain.ui" line="14"/>
+        <location filename="omain.cpp" line="86"/>
+        <source>Garden</source>
+        <translation>Ogród</translation>
+    </message>
+    <message>
+        <location filename="omain.ui" line="50"/>
+        <source>File</source>
+        <translation>Plik</translation>
+    </message>
+    <message>
+        <location filename="omain.ui" line="59"/>
+        <source>Options</source>
+        <translation>Opcje</translation>
+    </message>
+    <message>
+        <location filename="omain.ui" line="69"/>
+        <source>Open file</source>
+        <translation>Otwórz plik</translation>
+    </message>
+    <message>
+        <location filename="omain.ui" line="72"/>
+        <source>Ctrl+O</source>
+        <translation>Ctrl+O</translation>
+    </message>
+    <message>
+        <location filename="omain.ui" line="77"/>
+        <source>New file</source>
+        <translation>Nowy plik</translation>
+    </message>
+    <message>
+        <location filename="omain.ui" line="80"/>
+        <source>Ctrl+N</source>
+        <translation>Ctrl+N</translation>
+    </message>
+    <message>
+        <location filename="omain.ui" line="88"/>
+        <source>Edit built-ins</source>
+        <translation>Edytuj wartości wbudowane</translation>
+    </message>
+    <message>
+        <location filename="omain.ui" line="91"/>
+        <source>Ctrl+E</source>
+        <translation>Ctrl+E</translation>
+    </message>
+    <message>
+        <location filename="omain.ui" line="96"/>
+        <source>Close file</source>
+        <translation>Zamknij plik</translation>
+    </message>
+    <message>
+        <location filename="omain.ui" line="101"/>
+        <source>Quit</source>
+        <translation>Wyjście</translation>
+    </message>
+    <message>
+        <location filename="omain.ui" line="104"/>
+        <source>Ctrl+Q</source>
+        <translation>Ctrl+Q</translation>
+    </message>
+    <message>
+        <location filename="omain.cpp" line="51"/>
+        <source>Select garden file</source>
+        <translation>Wybierz plik z ogrodem</translation>
+    </message>
+    <message>
+        <location filename="omain.cpp" line="69"/>
+        <source>Unable to open file</source>
+        <translation>Nie można otworzyć pliku</translation>
+    </message>
+    <message>
+        <location filename="omain.cpp" line="70"/>
+        <source>Selected file cannot be opened</source>
+        <translation>Wybrany plik nie może zostać otwarty</translation>
+    </message>
+    <message>
+        <location filename="omain.cpp" line="114"/>
+        <source>New specimen</source>
+        <translation>Nowy gatunek</translation>
+    </message>
+</context>
+<context>
+    <name>QApplication</name>
+    <message utf8="true">
+        <location filename="oqueries.cpp" line="63"/>
+        <source>pnące</source>
+        <translation>pnące</translation>
+    </message>
+    <message utf8="true">
+        <location filename="oqueries.cpp" line="70"/>
+        <source>średni</source>
+        <translation>średni</translation>
+    </message>
+    <message utf8="true">
+        <location filename="oqueries.cpp" line="71"/>
+        <source>słaby</source>
+        <translation>słaby</translation>
+    </message>
+    <message utf8="true">
+        <location filename="oqueries.cpp" line="75"/>
+        <source>słabo powtarza</source>
+        <translation>słabo powtarza</translation>
+    </message>
+</context>
+<context>
+    <name>SpecialAttribute</name>
+    <message>
+        <location filename="specialattribute.ui" line="14"/>
+        <source>Form</source>
+        <translation>Formularz</translation>
+    </message>
+    <message>
+        <location filename="specialattribute.ui" line="20"/>
+        <source>Attribute</source>
+        <translation>Atrybut</translation>
+    </message>
+</context>
+</TS>
